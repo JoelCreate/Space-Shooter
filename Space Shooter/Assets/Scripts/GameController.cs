60e14f16d6b6e19deb704079bb30779b0dd00a23
JoelCreate <j.wiesner88@gmail.com>
2016-01-14 18:01:45 -0800

    Added score tracker - Joel

--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -9,9 +9,14 @@ public class GameController : MonoBehaviour
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+	public GUIText scoreText;
+
+	private int score;
 
 	void Start ()
 	{
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
 
@@ -29,5 +34,17 @@ public class GameController : MonoBehaviour
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+			
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score : " + score;
 	}
 }
