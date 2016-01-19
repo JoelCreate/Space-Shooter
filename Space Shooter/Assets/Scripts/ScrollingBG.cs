b68c5491ebc6bf5f9f22c745c394ef8f72ca5e29
JoelCreate <j.wiesner88@gmail.com>
2016-01-18 19:22:40 -0800

    Added scrolling BG - Joel

---
+++ Space Shooter/Assets/Scripts/ScrollingBG.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBG : MonoBehaviour {
+
+	public float speed = 0f;
+
+	void Update () 
+	{
+		GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (0f, speed * Time.time);
+	}
+
+}
